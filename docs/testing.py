f16da5a23541cd9e22a24ea174ebc0ba2578b92c
Dan Ruth <danjruth@gmail.com>
2017-05-16 13:55:47 -0600

    mutation and crossover

--- docs/testing.py
+++ docs/testing.py
@@ -7,23 +7,39 @@ Created on Sun May 14 17:30:14 2017
 
 import algorithm as ga
 
-rules_dict = {'a':{'min':0,'max':1},
-              'b':{'min':5,'max':12}}
+rules_dict = {'a':{'min':2,'max':3},
+              'b':{'min':0,'max':1},
+              'c':{'min':-1,'max':1},
+              'd':{'min':-5,'max':1}}
 my_rules = ga.member_rules(rules_dict)
 my_member = ga.member()
 my_member.rand_init(my_rules)
 
-print(my_rules.genes)
-print(my_member.genes)
-
 def fun_to_opt(genes):
-    return genes['a'] + genes['b']
+    return genes['a']**2 - genes['b']*genes['a'] + genes['c']*genes['d'] +  genes['b'] - genes['b']**2
 
-my_member.evaluate(fun_to_opt)
-print(my_member.score)
+my_pop = ga.population(my_rules,pop_size=100)
 
-my_pop = ga.population(my_rules)
-my_pop.evaluate(fun_to_opt)
-my_pop.sort_via_df()
+for i in range(50):
 
-print(my_pop.df)
+    my_pop.evaluate(fun_to_opt)
+    my_pop.sort_via_df()
+    #print(my_pop.df)
+    
+    print('Reinitializing...')
+    my_pop.reinit_some()
+    #print(my_pop.df)
+    
+    print('Mutating and crossover...')
+    my_pop.mutate_pop()
+    my_pop.cross_pop()
+    #print(my_pop.df)
+    
+    print('Back to list...')
+    my_pop.df_to_memberlist()
+    print(my_pop.df.head())
+    #for mem in my_pop.members:
+        #print(mem.genes)
+    
+    print('TOP SCORE:')
+    print(my_pop.df.loc[0,'score'])
--- evolve/algorithm.py
+++ evolve/algorithm.py
@@ -7,6 +7,7 @@ Created on Sun May 14 17:02:02 2017
 
 import random
 import pandas as pd
+import numpy as np
 
 class member_rules:
     '''
@@ -72,9 +73,12 @@ class population:
         members = [member() for m in range(pop_size)]
         [members[m].rand_init(rules) for m in range(pop_size)]
         self.members = members
+        self.rules = rules
         
     def evaluate(self,opt_fun):
         [mem.evaluate(opt_fun) for mem in self.members]
+        # now that it's evaluated, make it a tuple
+        self.members = tuple(self.members)
         
     def sort_via_df(self):
         
@@ -86,12 +90,102 @@ class population:
         for m,mem in enumerate(self.members):
             df.loc[m,'score'] = mem.score
         
+        df['has_changed'] = False
         df.sort_values('score',ascending=False,inplace=True)
         df.index = range(len(df.index))
             
         self.df = df
         
+    def reinit_some(self,reinit_frac=0.5):
+        '''
+        Delete and randomly reinitialize the worse-performing members.
+        '''
+        
+        df = self.df.copy()
+        
+        for ix in np.arange(round(len(self.members)*(1-reinit_frac)),len(self.members)):
+            new_member = member()
+            new_member.rand_init(self.rules)
+            new_genes = new_member.genes
+            df.loc[ix,:] = None
+            for gene in list(new_genes.keys()):
+                df.loc[ix,gene] = new_genes[gene]
+            df.loc[ix,'has_changed'] = True
+        
+        self.df = df        
+        
+    def mutate_pop(self,mut_frac=0.8,mut_prob=0.3,mut_num=0.2):
+        '''
+        Perform mutation on members of the population.
+        '''
+        
+        new_df = self.df.copy()
+        
+        for ix_float in np.arange(1,round(len(self.members)*mut_frac)):
+            
+            ix = int(ix_float)
+            mem = self.members[ix]
+            
+            if random.uniform(0,1) > mut_prob:
+                
+                # calculate the new value a gene will mutate to
+                gene_to_mut = random.choice(mem.rules.genes)
+                rel_mut_amount = (0.5*(float(ix) / float(len(self.members))))**1.5
+                mut_amount = rel_mut_amount * (mem.rules.gene_dict[gene_to_mut]['max'] - mem.rules.gene_dict[gene_to_mut]['min']) * np.random.normal()
+                new_val = mem.genes[gene_to_mut] + mut_amount
+                
+                # assign this value to both the member object and the dataframe
+                #print('   mutating '+str(self.members[ix].genes[gene_to_mut])+' to '+str(new_val))
+                self.members[ix].genes[gene_to_mut] = new_val
+                new_df.loc[ix,gene_to_mut] = new_val
+                
+        self.df = new_df
+        
+    def cross_pop(self,cross_frac=0.8,cross_prob=0.3,cross_num=0.2):
+        '''
+        Perform crossover on members of the population.
+        '''
+        
+        new_df = self.df.copy()
+        
+        for ix_float in np.arange(1,round(len(self.members)*cross_frac)):
+            
+            ix = int(ix_float)
+            mem = self.members[ix]
+            
+            if random.uniform(0,1) > cross_prob:
+                
+                parent_ix = random.choice(range(len(self.members)))
+                
+                # calculate the new value a gene will mutate to
+                gene_to_cross = random.choice(mem.rules.genes)
+                new_val = self.members[parent_ix].genes[gene_to_cross]
+                
+                # assign this value to both the member object and the dataframe
+                #print('   mutating '+str(self.members[ix].genes[gene_to_mut])+' to '+str(new_val))
+                self.members[ix].genes[gene_to_cross] = new_val
+                new_df.loc[ix,gene_to_cross] = new_val
+                
+        self.df = new_df
+                
     def df_to_memberlist(self):
+        
+        new_list = list()
+        
+        for m,mem in enumerate(self.members):
+            
+            # update the member's genes if they have changed
+            #if (self.df.loc[m,'has_changed'] == True):
+            if True:
+                #print(mem.genes)
+                for gene in mem.rules.genes:                    
+                    mem.genes[gene] = self.df.loc[m,gene]
+                #print(mem.genes)
+                #print('-------------')
+                    
+            new_list.append(mem)
+            
+        self.members = new_list         
             
 
 class genetic_algorithm:
